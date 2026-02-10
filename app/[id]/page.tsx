6719eed5939614d38deda0f0891c7b7a56d7191e
juanr.cifuentes <juanr.cifuentes@icloud.com>
2026-02-10 04:38:58 +0100

    Add `generateStaticParams` and update `_redirects` for dynamic routing
    - Added `generateStaticParams` to support static generation for dynamic routes.
    - Updated `_redirects` to handle dynamic routing with splat matching.

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,9 @@
 import PageSwiper from "@/app/components/PageSwiper";
 
+export function generateStaticParams() {
+  return [{ id: "_" }];
+}
+
 export default function Home() {
   return <PageSwiper initialSlide={0} />;
 }
