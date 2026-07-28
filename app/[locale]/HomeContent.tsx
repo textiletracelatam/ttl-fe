b34450b8a70565a9c61bb44cff00227d5c4a0d3b
Santiago Munera <sf.munera1130@gmail.com>
2026-07-28 12:37:06 -0500

    Swipe for home more visible (text-black)

--- app/[locale]/HomeContent.tsx
+++ app/[locale]/HomeContent.tsx
@@ -79,7 +79,7 @@ export default function HomeContent() {
 
       <div className="flex-1" />
 
-      <p className="lg:hidden sticky bottom-0 pb-6 pt-10 text-center text-lg tracking-widest uppercase text-neutral-400 dark:text-neutral-500 bg-linear-to-t from-background from-60% to-transparent">
+      <p className="lg:hidden sticky bottom-0 pb-6 pt-10 text-center text-lg tracking-widest uppercase text-black dark:text-neutral-500 bg-linear-to-t from-background from-60% to-transparent">
         {translation("swipe")} →
       </p>
 
